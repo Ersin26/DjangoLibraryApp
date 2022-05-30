47899c6a8c0c3919434eecc3b3ed700523a799c3
Ersin Sığınç <sigincersin@gmail.com>
2022-05-30 21:34:08 +0300

    Add celery configuration

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path(f'api/{API_PREFIX}/user/', include('apps.user.api.urls')),
-    # path(f'api/{API_PREFIX}/library/', include('apps.library.api.urls')),
+    path(f'api/{API_PREFIX}/library/', include('apps.library.api.urls')),
 ]
